f1c535eb4fad83e1e59efd480eccad01ec53517f
Alcides Rivera <elarsi626@gmail.com>
2026-01-03 15:41:32 +0000

    Hola

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [tarea, setTarea] = useState("");
+  const [tareas, setTareas] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+  const handleEnter = (e) => {
+    if(e.key !=="Enter") return;
+
+    if (tarea.trim() == "") {
+      alert("Rellena los campos")
+      return;
+    }
+    setTareas([...tareas, { tarea: tarea }])
+    setTarea("")
+  }
+
+
+  const handleDelete = (indexToDelete) => {
+  setTareas((tareasActuales) =>
+    tareasActuales.filter((_, index) => index !== indexToDelete)
+  );
+};
+
+
+
+
+
+
+  return (
+    <div className="text-center">
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={tarea}
+        onChange={(e) => setTarea(e.target.value)}
+        onKeyUp={handleEnter}
+        name="tareas"
+      />
+      <div className="container d-flex">
+        {
+          tareas.map((tarea, index) => (
+            <div key={index}>
+              <p>{tarea.tarea}</p>
+              <button onClick={()=>handleDelete(index)}>X</button>
+            </div>
+          )
+          )
+        }
+      </div>
+    </div>
+  );
 };
 
 export default Home;
